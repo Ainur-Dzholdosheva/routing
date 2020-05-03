a6cb535f6b63c8659981358babd8eafc9ecae539
Ainur Dzholdosheva <ajnurdzoldoseva@gmail.com>
2020-05-03 21:29:36 +0600

    install router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ul>
+        <li>Home</li>
+        <li>About me</li>
+        <li>Services</li>
+      </ul>
+      <section>
+        <h1>Home</h1>
+      </section>
+      <section>
+        <h1>About me</h1>
+      </section>
+      <section>
+        <h1>Serices</h1>
+      </section>
+    </div>
+  );
 }
 
 export default App;
